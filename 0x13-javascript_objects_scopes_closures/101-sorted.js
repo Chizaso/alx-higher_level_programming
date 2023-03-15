93b535ddad23ec2f1c912b110c3e7fffcf050b13
chizaso <johnnkhata@gmail.com>
2023-03-15 06:19:57 -0700

    done

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,14 +1,20 @@
 #!/usr/bin/node
-const dict = require('./101-data.js').dict;
-
+/*
+Write a script that imports a dictionary of occurrences by user id and computes a dictionary of user ids by occurrence.
+Your script must import dict from the file 101-data.js
+In the new dictionary:
+A key is a number of occurrences
+A value is the list of user ids
+Print the new dictionary at the end
+*/
+const dict = require('./101-data').dict;
 const newDict = {};
 
-Object.getOwnPropertyNames(dict).forEach(occurences => {
-  if (newDict[dict[occurences]] === undefined) {
-    newDict[dict[occurences]] = [occurences];
-  } else {
-    newDict[dict[occurences]].push(occurences);
+for (const key in dict) {
+  if (typeof (newDict[dict[key]]) === 'undefined') {
+    newDict[dict[key]] = [];
   }
-});
+  newDict[dict[key]].push(key);
+}
 console.log(newDict);
 
